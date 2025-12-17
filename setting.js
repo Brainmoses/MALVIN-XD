78cb95216fc0d6fa14ee3935cbe87e283d6e0b07
Virus <yobramoses690@gmail.com>
2025-12-17 09:33:57 +0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,9 +1,9 @@
-//=====🏅 ᴍᴀʟᴠɪɴ  xᴅ🔥====
+//=====🏅Virus-254-king🔥====
 
 module.exports = {
-  BOT_NAME: 'ᴍᴀʟᴠɪɴ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263714757857', // add your phone number
+  BOT_NAME: 'Virus-254-king', // ur bot name
+  OWNER_NAME: 'Virus-254-king', //your name
+  OWNER_NUMBER: '254710329917', // add your phone number
   SESSION_ID: '', // add your session ID
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare' // ur timezone
